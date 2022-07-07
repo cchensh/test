9b48dc43f898572f65b54fe291765d025c9a3177
Cheng Chen <cheng.chen@slack-corp.com>
2022-07-06 22:12:17 -0400

    Update test.sh

--- test.sh
+++ test.sh
@@ -31,7 +31,7 @@ install_slack_cli() {
                                 fi
 
                                 slack_cli_name="slack-dev"
-                                if [ $# -gt 0 ]; then
+                                if [ $# -gt 1 ]; then
                                         shift $(($OPTIND - 1))
                                         slack_cli_name=${1}
                                 fi
