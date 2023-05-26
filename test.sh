548d935eb362b49ab73a0fd501acbbabf0db4240
Cheng Chen <35309699+cchensh@users.noreply.github.com>
2023-05-26 16:55:46 +0000

    Add files via upload

---
+++ test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [ $# -gt 0 ]; then
+    token=${1}
+    # loc trigger create --trigger-def triggers/greeting_trigger.ts --token "${{ secrets.SLACK_TOKEN }}"
+    hermes-dev login --auth $token
+    sleep 3
+    hermes-dev trigger create --trigger-def triggers/greeting_trigger.ts --token $token 
+    sleep 3
+    xdotool key down
+    sleep 3
+    xdotool key --clearmodifiers Return 
+fi
